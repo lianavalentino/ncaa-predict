7d5c3ed97e460aca7fc48da4378beeafd5778a1c
Liana Valentino <lianavalentino@aol.com>
2024-12-09 01:58:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,49 +8,6 @@ PROJECT_ID = "ncaambb-443717"
 DATASET_ID = "march_madness"
 TEAM_STATS_TABLE = f"{PROJECT_ID}.{DATASET_ID}.teams_data"
 
-@app.route('/train', methods=['POST'])
-def train():
-    client = bigquer.Client()
-    query - """
-    CREATE OR REPLACE MODEL
-  `march_madness.model`
-    OPTIONS
-      ( model_type='logistic_reg') AS
-    
-    SELECT
-    
-      team1Win AS label,
-      team1_Wp,
-      team2_Wp
-      #our offensive four factors
-      team1_eFGp,
-      team1_TOVp,
-      team1_ORBp,
-      team1_FTR,
-      #our defensive four factores
-      team1_opp_eFGp,
-      team1_opp_TOVp,
-      team1_opp_ORBp,
-      team1_opp_FTR,
-        #opp offensive four factors
-      team2_eFGp,
-      team2_TOVp,
-      team2_ORBp,
-      team2_FTR,
-    
-      #opp defensive four factores
-      team2_opp_eFGp,
-      team2_opp_TOVp,
-      team2_opp_ORBp,
-      team2_opp_FTR,
-    
-    FROM `march_madness.tournament_games_data`
-    
-    # here we'll train on 2013 - 2033 and predict on 2023
-    WHERE Season BETWEEN 2013 AND 2022 
-
-    """
-    return jsonify({"message": "Model training completed and saved in GCP"}), 200
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -66,21 +23,37 @@ def predict():
       predicted_label_probs
     FROM ML.PREDICT(MODEL `march_madness.model`,
       (
-    SELECT
-    t1.*,
-    t2.Team as team2,
-    t2.eFGp AS team2_eFGp,
-    t2.TOVp AS team2_TOVp,
-    t2.ORBp AS team2_ORBp,
-    t2.FTR AS team2_FTR,
-    t2.opp_eFGp AS team2_opp_eFGp,
-    t2.opp_TOVp AS team2_opp_TOVp,
-    t2.opp_ORBp AS team2_opp_ORBp,
-    t2.opp_FTR AS team2_opp_FTR,
-        FROM `{TEAM_STATS_TABLE}` t1
-        JOIN `{TEAM_STATS_TABLE}` t2
-        ON t1.Team = '{team1}' AND t2.Team = '{team2}' AND t1.Season = t2.Season
+
+        SELECT
+          t1.Season,
+          t1.Team AS team1,
+          t2.Team AS team2,
+          -- Team1 stats
+          t1.eFGp AS team1_eFGp,
+          t1.TOVp AS team1_TOVp,
+          t1.ORBp AS team1_ORBp,
+          t1.FTr AS team1_FTr,
+          t1.opp_eFGp AS team1_opp_eFGp,
+          t1.opp_TOVp AS team1_opp_TOVp,
+          t1.opp_ORBp AS team1_opp_ORBp,
+          t1.opp_FTr AS team1_opp_FTr,
+          t1.Wp AS team1_Wp,
+          -- Team2 stats
+          t2.eFGp AS team2_eFGp,
+          t2.TOVp AS team2_TOVp,
+          t2.ORBp AS team2_ORBp,
+          t2.FTr AS team2_FTr,
+          t2.opp_eFGp AS team2_opp_eFGp,
+          t2.opp_TOVp AS team2_opp_TOVp,
+          t2.opp_ORBp AS team2_opp_ORBp,
+          t2.opp_FTr AS team2_opp_FTr,
+          t2.Wp AS team2_Wp
+        FROM 
+          `ncaambb-443717.march_madness.teams_data` AS t1
+          JOIN `ncaambb-443717.march_madness.teams_data` AS t2
+          ON t1.Team = '{team1}' AND t2.Team = '{team2}' AND t1.Season = t2.Season
         WHERE t1.Season = {season}
+        
       ))
     """
     query_job = client.query(query)
